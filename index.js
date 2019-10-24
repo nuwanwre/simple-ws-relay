d2ea752b1a8eb40acf66e63e01c4018ca4467da6
nuwanwre <nuwanwre@live.com>
2019-10-24 19:20:32 +0900

    fix: client popping on clonnection close

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ wsServer.on('request', function(request) {
 
     connection.on('close', function(connection) {
         if (index > -1) {
-            console.log((new Date()) + `: Connection closed for client: ${index}\nClients connected: ${clients.length}`)
+            console.log((new Date()) + `: Connection closed for client: ${index}\nClients connected: ${clients.length-1}`)
             clients.splice(index, 1);
         }
     });
